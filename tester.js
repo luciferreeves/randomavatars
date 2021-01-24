b1639decaa74cd2ae58505619dab2af4cff390ae
Priyansh <bobbyskhs@gmail.com>
2021-01-24 21:42:30 +0530

    Added testing file

---
+++ tester.js
@@ -0,0 +1,7 @@
+/**
+ * This file is only used for testing under development.
+ * Please do not consider this file to be a part of this code.
+ * There can be some code here which does not correspond to the the code at all.
+ */
+
+console.log('This works');
